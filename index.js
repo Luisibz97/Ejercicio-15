a703404ebac528e03c783c5f0270b8bd1b783fb2
Luisibz97 <106488695+Luisibz97@users.noreply.github.com>
2022-07-31 21:52:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const surname = "Ibañez";
 const nameAndSurname = {
     name: "Luis",
     surname: "Ibañez"
-}
+};
 
 function setCookie(name,value,minExpirate) {
     var expires = (new Date(Date.now() + minExpirate * 60000)).toUTCString();
     document.cookie = `${name}=${value}; expires=${expires}; path=/;`;
-}
+};
 
-setCookie('Person',JSON.stringify(nameAndSurname),2)
+setCookie('Person',JSON.stringify(nameAndSurname),2);
